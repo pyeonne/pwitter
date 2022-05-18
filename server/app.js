7049edb16fdce9684279a91476672b3034afe4d6
pyeonne <pyeonne@gmail.com>
2022-05-18 12:56:05 +0900

    📦 NEW: MySQL 적용

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 import { config } from './config.js';
 import { initSocket } from './connection/socket.js';
+import { db } from './database/database.js';
 
 const app = express();
 
@@ -27,5 +28,6 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
+db.getConnection().then((connection) => console.log(connection));
 const server = app.listen(config.host.port);
 initSocket(server);
