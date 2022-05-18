432925f136a3a7935f2d1c78f397b5b55e5cfc4f
pyeonne <pyeonne@gmail.com>
2022-05-18 14:05:41 +0900

    📦 NEW: Sequelize 적용

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,25 +1,44 @@
-// * abcd1234: $2b$15$9oMWpOjWjkH7TAvh4lWlteFoH1jihoi6.usudML1BLYwkxmkkJZ5i
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/database.js';
 
-import { db } from '../database/database.js';
+export const User = sequelize.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    url: DataTypes.TEXT,
+  },
+  { timestamps: false },
+);
 
 export async function findByUsername(username) {
-  return db
-    .execute('SELECT * FROM users WHERE username=?', [username])
-    .then((result) => result[0][0]);
+  return User.findOne({ where: { username } });
 }
 
 export async function findById(id) {
-  return db
-    .execute('SELECT * FROM users WHERE id=?', [id])
-    .then((result) => result[0][0]);
+  return User.findByPk(id);
 }
 
 export async function createUser(user) {
-  const { username, password, name, email, url } = user;
-  return db
-    .execute(
-      'INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
-      [username, password, name, email, url],
-    )
-    .then((result) => result[0].insertId);
+  return User.create(user).then((data) => data.dataValues.id);
 }
