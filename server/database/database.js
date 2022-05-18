b64dfd7d655c74f725136b6abbf775696f3a10bb
pyeonne <pyeonne@gmail.com>
2022-05-18 23:34:43 +0900

    👌 IMPROVE: Prep for Heroku deployment

--- server/database/database.js
+++ server/database/database.js
@@ -1,9 +1,16 @@
 import { config } from '../config.js';
 import { Sequelize } from 'sequelize';
 
-const { host, user, database, password } = config.db;
+const { host, port, user, database, password } = config.db;
 export const sequelize = new Sequelize(database, user, password, {
   host,
-  dialect: 'mysql',
+  port,
+  dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
